5ca3af7dff51cb6d163a7f9d0da18b3c270e843f
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-28 18:28:55 -0500

    completed landing

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -3,10 +3,13 @@ import homeCook from '../images/2729063.png'
 import restaurant from '../images/4483612.png'
 const Landing = () => {
   return (
-  <div id='landing'> 
-    <img id="homecook-landing-image"src={homeCook}/>
-    <img id="logo-landing-image"src={logo}/>
-    <img id="restaurant-landing-image"src={restaurant}/>
+  <div>
+    <div id='landing'> 
+      <img id="homecook-landing-image"src={homeCook}/>
+      <img id="logo-landing-image"src={logo}/>
+      <img id="restaurant-landing-image"src={restaurant}/>
+    </div>
+    <h1 id='landing-title'>the foodie network</h1>
   </div>
 )
 }
