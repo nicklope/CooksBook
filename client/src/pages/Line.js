105f8a698b37a7f9253c4836db888f3f0b50c553
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-29 08:47:34 -0500

    clicking the big logo navigates to the about page

--- client/src/pages/Line.js
+++ client/src/pages/Line.js
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Ticket from '../components/Ticket'
 import LineHeader from '../components/LineHeader'
