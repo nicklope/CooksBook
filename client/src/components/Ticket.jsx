a4cd3e19fb0388057526a40dce9cec42f0cea60f
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-29 07:00:00 -0500

    made ticket structure

--- client/src/components/Ticket.jsx
+++ client/src/components/Ticket.jsx
@@ -2,7 +2,22 @@ const Ticket = () => {
   return (
     <div id="ticket">
       <img src="https://i.imgur.com/m8TyWye.jpg"/>
-      <h1>Japanese Salmon Cubes</h1>
+      <div id="ticket-middle">
+        <h1 id="ticket-name">Japanese Salmon Cubes</h1>
+        <h3 id="ticket-overview"></h3>
+        <button id="view-ticket-button">View Full Ticket</button>
+      </div>
+      <div id="ticked-end">
+        <h1 id="ticket-ingredients-title">Ingredients</h1>
+        <ul id="ticket-ingredient-list">
+          <li>ingredient</li>
+          <li>ingredient</li>
+          <li>ingredient</li>
+        </ul>
+        <p>Tag 1</p>
+        <p>Tag 2</p>
+        <p>Tag 3</p>
+      </div>
     </div>
   )
 }
